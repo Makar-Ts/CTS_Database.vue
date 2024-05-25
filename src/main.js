d164533079b1800928badb71cf03088afcfa0605
Makar Ts <tsivikov.makar@gmail.com>
2024-05-25 19:21:17 +0300

    ded

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+
+Vue.config.productionTip = false
 
 const app = createApp(App)
 
